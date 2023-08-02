8ed8bed4d522b2ac7eed72066448ff84ff9bb860
Yevhenii Lymarenko <96422034+YevheniiLi@users.noreply.github.com>
2023-08-02 14:23:24 +0200

    create googlemap with autocomplete component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Map } from "./components/Map";
+import { useJsApiLoader } from "@react-google-maps/api";
+import s from "./App.module.css";
+import { Autocomplete } from "./components/Autocomplete";
+import React from "react";
 
-function App() {
+const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(API_KEY);
+
+const defaultCenter = {
+  lat: -3.745,
+  lng: -38.523,
+};
+
+const libraries = ["places"];
+
+const App = () => {
+  const [center, setCenter] = React.useState(defaultCenter);
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: API_KEY,
+    libraries,
+  });
+
+  const onPlaceSelect = React.useCallback((coordinates) => {
+    setCenter(coordinates);
+  }, [],);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={s.addressSearchContainer}>
+      <Autocomplete isLoaded={isLoaded} onSelect={onPlaceSelect} />
+      <button className={s.modeToggle}>Set markers</button>
+
+      </div>
+      {isLoaded ? <Map center={center} /> : <h2>Loading</h2>}
     </div>
   );
-}
+};
 
 export default App;
